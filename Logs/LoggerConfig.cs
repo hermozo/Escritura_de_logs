17bdf678ae42ef4b26fbc81bb0e741fe053d02a1
Angel Hermozo <mickyhermozo72@gmail.com>
2024-08-06 10:19:32 -0400

    Implemenatcion del Log

---
+++ Logs/LoggerConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Logs
+{
+    public static class LoggerConfig
+    {
+        private static Serilog.ILogger _logger;
+
+        public static void ConfigureLogger(IConfiguration configuration)
+        {
+            _logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        public static Serilog.ILogger Logger => _logger;
+    }
+}
